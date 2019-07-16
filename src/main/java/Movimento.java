633b6769f06ff977c8a16cd86c1cd12821042f2a
RodrigoSanches <rodrigbehsanchez@gmail.com>
2019-07-15 22:17:39 -0300

    Movimento class update

---
+++ src/main/java/Movimento.java
@@ -0,0 +1,14 @@
+
+
+public class Movimento {
+
+    public String getDataMovimento() {
+        return dataMovimento;
+    }
+
+    public void setDataMovimento(String dataMovimento) {
+        this.dataMovimento = dataMovimento;
+    }
+
+    private String dataMovimento;
+}
